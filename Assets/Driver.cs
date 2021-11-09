78365a85b0c65a6507d6a75bc63af87f3b92bb4d
Moesess <33929420+Moesess@users.noreply.github.com>
2021-11-09 17:33:59 +0100

    Player controls the car

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour{
+    
+    [SerializeField] float turnSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
     // Start is called before the first frame update
     void Start(){
         
@@ -10,6 +13,10 @@ public class Driver : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        
+        float turnVar = turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float moveVar = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Rotate(0, 0, -turnVar);
+        transform.Translate(0, moveVar, 0);
     }
 }
